93a2e78d5abef037047481821941db29c7c1749f
Cécile SEILLET <45199169+frofinours@users.noreply.github.com>
2021-06-02 23:08:30 +0200

    Question 4
    ** One bin per item solution
    **First Fit solution

---
+++ src/com/polytech/algorithm/Question4/OneBinPerItem.java
@@ -0,0 +1,38 @@
+package com.polytech.algorithm.Question4;
+
+import com.polytech.Bin;
+import com.polytech.Item;
+import com.polytech.Solution;
+import com.polytech.algorithm.Algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class OneBinPerItem implements Algorithm {
+    @Override
+    public Solution generateSolution(LinkedList<Item> listItems, int binSize) {
+
+        if (listItems == null) {
+            System.err.println("listItems is null, impossible to handle it");
+            return null;
+        }
+
+        if (binSize <= 0) {
+            System.err.println("binSize must be greater than 0");
+            return null;
+        }
+
+        LinkedList<Item> listItemsSorted = (LinkedList<Item>) listItems.clone();
+        ArrayList<Bin> listBins = new ArrayList<>();
+
+        // Create a bin per item and put the item in it
+        for (Item item : listItemsSorted
+             ) {
+            Bin newBin = new Bin(binSize);
+            listBins.add(newBin);
+            newBin.addItem(item);
+        }
+
+        return new Solution(listBins);
+    }
+}
