4f7be75cdaeacaed19f0d039c90ad7477346ed87
Marion Sinaeve <sinaeve.marion@gmail.com>
2021-06-04 15:37:23 +0200

    or-tools sans or-tool

---
+++ test/algorithm/ProgrammationLineaireTest.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import com.polytech.Solution;
+import com.polytech.algorithm.ProgrammationLineaire;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgrammationLineaireTest {
+
+    @Test
+    void test() {
+
+        ProgrammationLineaire programmationLineaire = new ProgrammationLineaire();
+        Solution solution = programmationLineaire.generateSolution(null, -1);
+    }
+
+}
