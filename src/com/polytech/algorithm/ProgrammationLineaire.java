e8a55727a100601685b11f9ec80428a4f8a8b534
Cécile SEILLET <45199169+frofinours@users.noreply.github.com>
2021-06-06 16:19:30 +0200

    Question 3 : Prog lin or-tools

--- src/com/polytech/algorithm/ProgrammationLineaire.java
+++ src/com/polytech/algorithm/ProgrammationLineaire.java
@@ -1,57 +1,87 @@
 package com.polytech.algorithm;
-
+import com.google.ortools.Loader;
 import com.google.ortools.linearsolver.MPConstraint;
 import com.google.ortools.linearsolver.MPObjective;
-import com.google.ortools.linearsolver.MPVariable;
 import com.google.ortools.linearsolver.MPSolver;
-
-import com.polytech.Item;
-import com.polytech.Solution;
-
-import java.util.LinkedList;
-
-public class ProgrammationLineaire implements Algorithm {
+import com.google.ortools.linearsolver.MPVariable;
 
 
-//    public static void ruc() {
-//
-//        // Create the linear solver with the GLOP backend.
-//        MPSolver solver;
-//        MPSolver.createSolver("GLOP");
-//
-//        // Create the variables x and y.
-//        MPVariable x = solver.makeNumVar(0.0, 1.0, "x");
-//        MPVariable y = solver.makeNumVar(0.0, 2.0, "y");
-//
-//        System.out.println("Number of variables = " + solver.numVariables());
-//
-//        // Create a linear constraint, 0 <= x + y <= 2.
-//        MPConstraint ct = solver.makeConstraint(0.0, 2.0, "ct");
-//        ct.setCoefficient(x, 1);
-//        ct.setCoefficient(y, 1);
-//
-//        System.out.println("Number of constraints = " + solver.numConstraints());
-//
-//        // Create the objective function, 3 * x + y.
-//        MPObjective objective = solver.objective();
-//        objective.setCoefficient(x, 3);
-//        objective.setCoefficient(y, 1);
-//        objective.setMaximization();
-//
-//        solver.solve();
-//        System.out.println("Solution:");
-//        System.out.println("Objective value = " + objective.value());
-//        System.out.println("x = " + x.solutionValue());
-//        System.out.println("y = " + y.solutionValue());
-//    }
-
-    @Override
-    public Solution generateSolution(LinkedList<Item> listItems, int binSize) {
-        Solution s = new Solution();
-
-
-//        ruc();
-
-        return s;
+public class ProgrammationLineaire {
+    static class DataModel {
+        public final double[] weights = {48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30};
+        public final int numItems = weights.length;
+        public final int numBins = weights.length;
+        public final int binCapacity = 100;
     }
+
+    public static void main(String[] args) throws Exception {
+        Loader.loadNativeLibraries();
+        final DataModel data = new DataModel();
+
+        // Create the linear solver with the SCIP backend.
+        MPSolver solver = MPSolver.createSolver("SCIP");
+        if (solver == null) {
+            System.out.println("Could not create solver SCIP");
+            return;
+        }
+
+        MPVariable[][] x = new MPVariable[data.numItems][data.numBins];
+        for (int i = 0; i < data.numItems; ++i) {
+            for (int j = 0; j < data.numBins; ++j) {
+                x[i][j] = solver.makeIntVar(0, 1, "");
+            }
+        }
+        MPVariable[] y = new MPVariable[data.numBins];
+        for (int j = 0; j < data.numBins; ++j) {
+            y[j] = solver.makeIntVar(0, 1, "");
+        }
+
+        double infinity = java.lang.Double.POSITIVE_INFINITY;
+        for (int i = 0; i < data.numItems; ++i) {
+            MPConstraint constraint = solver.makeConstraint(1, 1, "");
+            for (int j = 0; j < data.numBins; ++j) {
+                constraint.setCoefficient(x[i][j], 1);
+            }
+        }
+
+        for (int j = 0; j < data.numBins; ++j) {
+            MPConstraint constraint = solver.makeConstraint(0, infinity, "");
+            constraint.setCoefficient(y[j], data.binCapacity);
+            for (int i = 0; i < data.numItems; ++i) {
+                constraint.setCoefficient(x[i][j], -data.weights[i]);
+            }
+        }
+
+        MPObjective objective = solver.objective();
+        for (int j = 0; j < data.numBins; ++j) {
+            objective.setCoefficient(y[j], 1);
+        }
+        objective.setMinimization();
+
+        final MPSolver.ResultStatus resultStatus = solver.solve();
+
+        // Check that the problem has an optimal solution.
+        if (resultStatus == MPSolver.ResultStatus.OPTIMAL) {
+            System.out.println("Number of bins used: " + objective.value());
+            double totalWeight = 0;
+            for (int j = 0; j < data.numBins; ++j) {
+                if (y[j].solutionValue() == 1) {
+                    System.out.println("\nBin " + j + "\n");
+                    double binWeight = 0;
+                    for (int i = 0; i < data.numItems; ++i) {
+                        if (x[i][j].solutionValue() == 1) {
+                            System.out.println("Item " + i + " - weight: " + data.weights[i]);
+                            binWeight += data.weights[i];
+                        }
+                    }
+                    System.out.println("Packed bin weight: " + binWeight);
+                    totalWeight += binWeight;
+                }
+            }
+            System.out.println("\nTotal packed weight: " + totalWeight);
+        } else {
+            System.err.println("The problem does not have an optimal solution.");
+        }
+    }
+    private ProgrammationLineaire() {}
 }
--- test/algorithm/ProgrammationLineaireTest.java
+++ test/algorithm/ProgrammationLineaireTest.java
@@ -8,14 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgrammationLineaireTest {
 
+    static {
+        System.loadLibrary("jniortools");
+    }
+
     @Test
     void test() {
 
-//        System.loadLibrary("jniortools");
+        System.loadLibrary("jniortools");
 
-
+/*
         ProgrammationLineaire programmationLineaire = new ProgrammationLineaire();
         Solution solution = programmationLineaire.generateSolution(null, -1);
+
+ */
     }
 
 
